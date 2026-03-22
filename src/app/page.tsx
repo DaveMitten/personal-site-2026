9ab66ec47d56086889e7e703f4b9d6892f9cfef7
Dave Mitten <davidmitten88@gmail.com>
2026-03-22 10:23:25 +0000

    pushing to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <article>
           <h1 className="text-4xl font-bold">Hi, I'm Dave,</h1>
-
           <br />
           {/* build things can be a link to the github projects */}
           <p>I'm a developer and I like to <a className="underline" href="https://github.com/DaveMitten" target="_blank" rel="noopener noreferrer">build things</a>.</p>
           <p>I have been building with code for over 9 years now.</p>
           <br />
+
           <p>I like writing <strong>good</strong>, <strong>clean</strong> code. <em>Easy-to-read</em> code.
             <br />Code that is <em>easy to understand</em> and <em>easy to maintain</em>. </p>
           <p>I like good processes. Reducing the pains that come with the writing of said code.</p>
@@ -26,7 +26,7 @@ export default function Home() {
           </p>
           <p>If you want to chat, <a className="underline" href="https://www.linkedin.com/in/davidjamesmitten/" target="_blank" rel="noopener noreferrer">reach me here</a>.</p>
           <br />
-          <p>I also cook, eat, watch docs on physics and the state of the world. Music used to be my full time world. </p>
+          <p>Music used to be my full time world. </p>
         </article>
       </main>
     </div >
